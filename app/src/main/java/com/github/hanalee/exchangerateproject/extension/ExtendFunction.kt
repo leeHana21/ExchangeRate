7dc52b3977972563bbe4a83c0ccbdfc09b9c388c
leeHana21 <leehn0699@gmail.com>
2022-02-21 14:08:29 +0900

    Fix : 미사용 메서드 삭제

--- app/src/main/java/com/github/hanalee/exchangerateproject/extension/ExtendFunction.kt
+++ app/src/main/java/com/github/hanalee/exchangerateproject/extension/ExtendFunction.kt
@@ -5,26 +5,6 @@ import android.view.View
 import android.widget.Toast
 
 
-fun View.toVisible() {
-    this.visibility = View.VISIBLE
-}
-
-fun View.toInvisible() {
-    this.visibility = View.INVISIBLE
-}
-
-fun View.toGone() {
-    this.visibility = View.GONE
-}
-
-fun View.toEnable() {
-    this.isEnabled = true
-}
-
-fun View.toDisable() {
-    this.isEnabled = false
-}
-
 fun Context.toToast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
